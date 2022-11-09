30528f5dc5e693b983c91c688873e97a7a6a1d1b
1e10 <sample@bths.edu>
2022-11-09 11:00:45 -0500

    new

--- src/Main.java
+++ src/Main.java
@@ -1,150 +1,187 @@
 public class Main {
     public static void main(String[] args){
-        for (int i = 1; i <= 10; i ++){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (int i = 1; i < 11; i ++){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (int i = 15; i >= 0; i --){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (int i = 15; i > -1; i --){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (int i = 1; i <= 15; i ++){
-            if (i % 2 != 0){
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();
-
-        for (int i = 1; i <= 15; i += 2){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-
-        for (int count = 5; count < 10; count++) {
-            System.out.print(count + " ");
-        }
-
-        for (int i = 20; i >= 0; i = i - 3) {
-            System.out.print(i + " ");
-        }
-
-        int sum = 0;
-        int x = 5;
-        for (int i = 0; i < 10 + x; i += 2) {
-            sum += i;
-        }
-        System.out.println(sum);
-
-        System.out.println("-----------------------------------");
-
-        //number 2
-
-        //a
-        int count1 = 5;
-        while (count1 < 10){
-            System.out.print(count1 + " ");
-            count1 ++;
-        }
-        System.out.println();
-
-        //b
-        int count2 = 20;
-        while (count2 >= 0){
-            System.out.print(count2 + " ");
-            count2 -= 3;
-        }
-        System.out.println();
-
-        //c
-        for (int i = 0; i <= 100; i += 5){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (int i = 10; i <= 50; i += 5){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-
-        System.out.println("-----------------------------------");
-        for (int i = 4; i < 12; i ++){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (int i = 4; i <= 11; i ++){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-
-        System.out.println("-----------------------------------");
-        for (int i = 4; i <= 14; i += 2){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (int i = 4; i <= 14; i ++){
-            if (i % 2 == 0) {
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();
-
-        System.out.println("-----------------------------------");
-        for (int i = 6; i <= 100; i += 6){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (int i = 0; i <= 100; i += 6){
-            if (i != 0) {
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();
-
-        int x1 = 10;
-        for (int i = 0; i < x; i++) {
-            System.out.println(i);
-            x1--;
-        }
-
-
-        int num = 100;
-        for (int count = 1; count <= num; count++)
-        {
-            System.out.print(num + " ");
-            num = num / 2;
-        }
-
-        System.out.println("-----------------------------------");
-
-
-        System.out.println("-----------------------------------");
+//        for (int i = 1; i <= 10; i ++){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//        for (int i = 1; i < 11; i ++){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//        for (int i = 15; i >= 0; i --){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//        for (int i = 15; i > -1; i --){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//        for (int i = 1; i <= 15; i ++){
+//            if (i % 2 != 0){
+//                System.out.print(i + " ");
+//            }
+//        }
+//        System.out.println();
+//
+//        for (int i = 1; i <= 15; i += 2){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//
+//        for (int count = 5; count < 10; count++) {
+//            System.out.print(count + " ");
+//        }
+//
+//        for (int i = 20; i >= 0; i = i - 3) {
+//            System.out.print(i + " ");
+//        }
+//
+//        int sum = 0;
+//        int x = 5;
+//        for (int i = 0; i < 10 + x; i += 2) {
+//            sum += i;
+//        }
+//        System.out.println(sum);
+//
+//        System.out.println("-----------------------------------");
+//
+//        //number 2
+//
+//        //a
+//        int count1 = 5;
+//        while (count1 < 10){
+//            System.out.print(count1 + " ");
+//            count1 ++;
+//        }
+//        System.out.println();
+//
+//        //b
+//        int count2 = 20;
+//        while (count2 >= 0){
+//            System.out.print(count2 + " ");
+//            count2 -= 3;
+//        }
+//        System.out.println();
+//
+//        //c
+//        for (int i = 0; i <= 100; i += 5){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//        for (int i = 10; i <= 50; i += 5){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//
+//        System.out.println("-----------------------------------");
+//        for (int i = 4; i < 12; i ++){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//        for (int i = 4; i <= 11; i ++){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//
+//        System.out.println("-----------------------------------");
+//        for (int i = 4; i <= 14; i += 2){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//        for (int i = 4; i <= 14; i ++){
+//            if (i % 2 == 0) {
+//                System.out.print(i + " ");
+//            }
+//        }
+//        System.out.println();
+//
+//        System.out.println("-----------------------------------");
+//        for (int i = 6; i <= 100; i += 6){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//
+//        for (int i = 0; i <= 100; i += 6){
+//            if (i != 0) {
+//                System.out.print(i + " ");
+//            }
+//        }
+//        System.out.println();
+//
+//        int x1 = 10;
+//        for (int i = 0; i < x; i++) {
+//            System.out.println(i);
+//            x1--;
+//        }
+//
+//
+//        int num = 100;
+//        for (int count = 1; count <= num; count++)
+//        {
+//            System.out.print(num + " ");
+//            num = num / 2;
+//        }
+//
+//        System.out.println("-----------------------------------");
         StringLoops loops = new StringLoops();
-        System.out.println(loops.countCharacters("a", "Apple and banana"));
-        System.out.println(loops.countCharacters("A", "Apple and banana"));
-        System.out.println(loops.countCharacters("!", "Hello! Nice day!"));
-        System.out.println(loops.reverseString("hello!"));
-        System.out.println(loops.reverseString("Apples and bananas"));
 
-        System.out.println("-----------------------------------");
-        System.out.println(loops.reverseString2("hello!"));
-        System.out.println(loops.reverseString2("Apples and bananas"));
+//        System.out.println("-----------------------------------");
+
+//        System.out.println(loops.countCharacters("a", "Apple and banana"));
+//        System.out.println(loops.countCharacters("A", "Apple and banana"));
+//        System.out.println(loops.countCharacters("!", "Hello! Nice day!"));
+//        System.out.println(loops.reverseString("hello!"));
+//        System.out.println(loops.reverseString("Apples and bananas"));
+//
+//        System.out.println("-----------------------------------");
+//        System.out.println(loops.reverseString2("hello!"));
+//        System.out.println(loops.reverseString2("Apples and bananas"));
+
+        System.out.println(loops.isPalindrome("mom"));
+        System.out.println(loops.isPalindrome("dad"));
+        System.out.println(loops.isPalindrome("bob"));
+        System.out.println(loops.isPalindrome("noon"));
+        System.out.println(loops.isPalindrome("deed"));
+        System.out.println(loops.isPalindrome("12321"));
+        System.out.println(loops.isPalindrome("aaaaaaa"));
+        System.out.println(loops.isPalindrome("baaabbaaab"));
+        System.out.println(loops.isPalindrome("civic"));
+        System.out.println(loops.isPalindrome("kayak"));
+        System.out.println(loops.isPalindrome("racecar"));
+        System.out.println(loops.isPalindrome("my gym"));
+        System.out.println(loops.isPalindrome("step on no pets"));
+        System.out.println(loops.isPalindrome("45TTTT54"));
+        System.out.println(loops.isPalindrome("no lemon no melon"));
+        System.out.println(loops.isPalindrome("a nut for a jar of tuna"));
+        System.out.println(loops.isPalindrome("! 1 2 3 4 M 6M4321!"));
+        System.out.println(loops.isPalindrome("was it a car or a cat i saw"));
+        System.out.println(loops.isPalindrome("a man a plan a canal panama"));
+        System.out.println("-------");
+        System.out.println(loops.isPalindrome("mod"));
+        System.out.println(loops.isPalindrome("dang"));
+        System.out.println(loops.isPalindrome("boo"));
+        System.out.println(loops.isPalindrome("noonish"));
+        System.out.println(loops.isPalindrome("indeed"));
+        System.out.println(loops.isPalindrome("1234432"));
+        System.out.println(loops.isPalindrome("civics"));
+        System.out.println(loops.isPalindrome("kayaks"));
+        System.out.println(loops.isPalindrome("racing car"));
+        System.out.println(loops.isPalindrome("my gyms"));
+        System.out.println(loops.isPalindrome("step on pets"));
+        System.out.println(loops.isPalindrome("1 3 2 4 3 2 1"));
+        System.out.println(loops.isPalindrome("AAADAA"));
+        System.out.println(loops.isPalindrome("BaaBaaaB"));
+
+
     }
 }
--- src/StringLoops.java
+++ src/StringLoops.java
@@ -54,6 +54,132 @@ public class StringLoops
         return newStr;
     }
 
+    /* Returns a String with all instances of "searchString" removed from "origString";
+       matches should be case sensitive (i.e. no need to worry about lowercase)
+
+     - if searchString = "an" and origString = "canned beans and bananas",
+       this method returns "Apples d bas"
+     - if searchString = "tat" and origString = "Ratatattat",
+       this method returns "Raat"
+     - if searchString = "lower" and origString = "sunflower",
+       this method returns "sunf"
+     - if searchString = "haha" and origString = "Hahahahaha",
+       this method returns "Ha" (case sensitive, so leading "Haha" isn’t a match)
+     - if searchString = "HAHA" and origString = "Hahahahaha",
+       this method returns "Hahahahaha" (no matches due to case)
+     - if searchString = "rain" and origString = "it’s the brain drain pain train",
+       this method returns "it’s the b d pain t"
+     - if searchString = "lol" and origString = "lolololol",
+       this method returns "ool"
+     - if searchString = "was" and origString = "I was about to call you, wasn’t I?",
+       this method returns "I  about to call you, n’t I?"
+     - if searchString = "but" and origString = "I was about to call you, wasn’t I?",
+       this method returns "I was about to call you, wasn’t I?" (no matches)
+     - if searchString = "AB" and origString = "ABABABABABC",
+       this method returns "C"
+     - if searchString = "AB" and origString = "AAAAABBBBBC",
+       this method returns "C"
+    */
+    public String removeString(String searchString, String origString) {
+        int lenSerStr = searchString.length();
+            int loc = origString.indexOf(searchString);
+            while(origString.contains(searchString)){
+                origString = origString.substring(0, loc) + origString.substring(loc + lenSerStr);
+
+                loc = origString.indexOf(searchString);
+            }
+        return origString;
+    }
+
+    /* Prints all integers from fromNum through toNum, separated by
+     a commas and a space, on a single line;
+     there should NOT be a comma after the final number.
+     If fromNum == toNum, print just the number and no comma.
+     If fromNum > toNum, the numbers should be printed in descending order.
+     After all numbers are printed, move the cursor to the next line.
+
+     Examples:
+     - if fromNum = 0 and toNum = 5, this method should print: 0, 1, 2, 3, 4, 5
+     - if fromNum = -3 and toNum = 2, this method should print: -3, -2, -1, 0, 2
+     - if fromNum = 8 and toNum = 1, this method should print: 8, 7, 6, 5, 4, 3, 2, 1
+     - if fromNum = 4 and toNum = 4, this method should print: 4
+     - if fromNum = 0 and toNum = 0, this method should print: 0
+    */
+    public void commaSeparated(int fromNum, int toNum) {
+        if (fromNum == toNum){
+            System.out.println(fromNum);
+        } else if (fromNum > toNum){
+            String str = "";
+            for (int i = fromNum; i >= toNum; i--) {
+                if (i != toNum) {
+                    str += i + ", ";
+                } else {
+                    str += i;
+                }
+            }
+            System.out.println(str);
+        }
+        else {
+            String str = "";
+            for (int i = fromNum; i <= toNum; i++) {
+                if (i != toNum) {
+                    str += i + ", ";
+                } else {
+                    str += i;
+                }
+            }
+            System.out.println(str);
+        }
+    }
+
+    /* Returns true if myString is a palindrome, i.e. the characters read the same
+    forwards and backwards, and false otherwise. Don't worry about case.
+    NOTE!  Spaces should be disregarded when determining if it's a palindrome.
+    "no lemon no melon" is a palindrome!
+
+     Examples:
+     - myString = "mom", return true
+     - myString = "dad", return true
+     - myString = "bob", return true
+     - myString = "noon", return true
+     - myString = "deed", return true
+     - myString = "12321", return true
+     - myString = "aaaaaaa", return true
+     - myString = "baaabaaab", return true
+     - myString = "civic", return true
+     - myString = "kayak", return true
+     - myString = "racecar", return true
+     - myString = "my gym", return true
+     - myString = "step on no pets", return true
+     - myString = "45TTGTT54", return true
+     - myString = "no lemon no melon", return true
+     - myString = "a nut for a jar of tuna", return true
+     - myString = "! 1 2 3 4 M 6M4321!", return true
+     - myString = "was it a car or a cat i saw", return true
+     - myString = "a man a plan a canal panama", return true
+     - myString = "mod", return false
+     - myString = "dang", return false
+     - myString = "boo", return false
+     - myString = "noonish", return false
+     - myString = "indeed", return false
+     - myString = "1234432", return false
+     - myString = "civics", return false
+     - myString = "kayaks", return false
+     - myString = "racing car", return false
+     - myString = "my gyms", return false
+     - myString = "step on pets", return false
+     - myString = "1 3 2 4 3 2 1", return false
+     - myString = "AAADAA", return false
+     - myString = "BaaBaaaB", return false
+    */
+    public boolean isPalindrome(String myString) {
+        StringLoops strNoSpace = myString.replace(" ", "");
+        String strReversed = reverseString(myString);
+        return strReversed.equals(myString);
+    }
+
+
+
 }
 
 
